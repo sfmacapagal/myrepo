84a409bff636d153c456ac7ad760ff6e15ca07f9
Steven Macapagal <sf.macapagal@gmail.com>
2022-06-07 18:22:05 -0500

    add header template
    1. add header.R

---
+++ header.R
@@ -0,0 +1,12 @@
+################################################################################
+# Filename: header.R                                                           #
+# Path: ./header.R                                                             #
+# Author: Steven Macapagal                                                     #
+# Date created: 2022-06-07                                                     #
+# Date modified: 2022-06-07                                                    #
+# Purpose: This script stores the header template.                             #
+# Inputs:                                                                      #
+# Outputs:                                                                     #
+# Notes:                                                                       #
+################################################################################
+
